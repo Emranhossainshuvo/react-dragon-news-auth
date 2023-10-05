2367a5995b7b31d5d18f3ddb78422541b99a1b35
emran <shuvoorba28@gmail.com>
2023-10-05 19:08:34 +0600

    login form added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,7 +5,33 @@ const Login = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <h2 className="text-3xl">This is login</h2>            
+            <h2 className="text-3xl">This is login</h2>
+            <div className="hero min-h-screen bg-base-200">
+                
+                     
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                   
+                
+            </div>
         </div>
     );
 };
