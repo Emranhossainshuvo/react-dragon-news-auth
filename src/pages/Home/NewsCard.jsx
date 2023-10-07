b509ead7dedbe0290593f16e43bd9fbc1194aaaf
emran <shuvoorba28@gmail.com>
2023-10-07 08:42:00 +0600

    many things added

--- src/pages/Home/NewsCard.jsx
+++ src/pages/Home/NewsCard.jsx
@@ -1,9 +1,19 @@
 
 
-const NewsCard = () => {
+const NewsCard = ({ news }) => {
+
+    const { title, thumbnail_url } = news;
+
     return (
-        <div>
-            
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={thumbnail_url } /></figure>
+            <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
